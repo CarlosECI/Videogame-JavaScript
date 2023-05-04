7e1ecdb4259007a234e561df9628a3ded3bd9db0
Carlos Castillo <carlosedcaib@gmail.com>
2023-05-04 15:29:01 -0400

    Se le dio formato al canvas para que tenga un tamaño estandar, pero relativo, segun el tamaño de la pantalla o si es esta cambia sin tener que recargar el juego de nuevo. Se  agregó un primer render del inicio del juego

--- game.js
+++ game.js
@@ -1,7 +1,35 @@
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+window.addEventListener('load', setCanvasSize);
+// Para evitar tener que recargar el juego cada vez que la pantalla cambie de tamaño, agregago un eventListener para que se haga el cálculo automático del tamaño de la pantalla y de esta manera sea más responsive.
+window.addEventListener('resize', setCanvasSize);
+
+let canvasSize;
+let elementsSize;
+
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.75;
+    } else {
+        canvasSize = window.innerHeight * 0.75;
+    }
+    canvas.setAttribute('width', canvasSize)
+    canvas.setAttribute('height', canvasSize)
+    
+    elementsSize = canvasSize / 10;
+
+    startGame();
+}
 
 function startGame() {
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = "end";
+
+    for (let i = 1; i <= 10; i++) {
+        for (let z = 1; z <= 10; z++) {
+            game. fillText(emojis['X'], elementsSize * i, elementsSize * z);
+        }
+    }
 }
