29a06bd588227b1f2d8e30f453db0be4038bc7a6
Carlos Castillo <carlosedcaib@gmail.com>
2023-05-04 22:48:05 -0400

    Se agregaron los addEventListener necesarios y las funciones para saber cuando el juegador quiere moverse tanto desde los botones como desde el teclado

--- game.js
+++ game.js
@@ -1,13 +1,55 @@
+const btnUp = document.getElementById('up')
+const btnLeft = document.getElementById('left')
+const btnRight = document.getElementById('right')
+const btnDown = document.getElementById('down')
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 
 window.addEventListener('load', setCanvasSize);
 // Para evitar tener que recargar el juego cada vez que la pantalla cambie de tamaño, agregago un eventListener para que se haga el cálculo automático del tamaño de la pantalla y de esta manera sea más responsive.
 window.addEventListener('resize', setCanvasSize);
+window.addEventListener('keydown', moveKeyboard)
+
+btnUp.addEventListener('click', moveUp)
+btnLeft.addEventListener('click', moveLeft)
+btnRight.addEventListener('click', moveRight)
+btnDown.addEventListener('click', moveDown)
+
+function moveUp() {
+    console.log('Moverse arriba');
+}
+
+function moveLeft() {
+    console.log('Moverse a la izquierda');
+}
+
+function moveRight() {
+    console.log('Moverse a la derecha');
+}
+
+function moveDown() {
+    console.log('Moverse abajo');
+}
+
+function moveKeyboard (event) {
+    switch (event.key) {
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight":
+            moveRight();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+    }
+}
 
 let canvasSize;
 let elementsSize;
-
 
 function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
@@ -37,8 +79,8 @@ function startGame() {
     mapCols.forEach((row, rowI) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col];
-            const posX = elementsSize * (colI + 1);
-            const posY = elementsSize * (rowI + 1);
+            const posX = elementsSize * (colI + 1) + 7;
+            const posY = elementsSize * (rowI + 1) - 7;
             game.fillText(emoji, posX, posY);
         })
     })
