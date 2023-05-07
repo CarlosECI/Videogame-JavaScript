9420bcf8a527cf8b4652dab167bda2680f524ee6
Carlos Castillo <carlosedcaib@gmail.com>
2023-05-07 13:18:38 -0400

    Se agrego el jugador y se le dio movimiento con las teclas y botones. Se creo un condicional para que no se duplique al moverlo y se limito el movimiento para que no salga de los limites del juego

--- game.js
+++ game.js
@@ -5,48 +5,14 @@ const btnDown = document.getElementById('down')
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 
+const playerPosition = {
+    x: undefined,
+    y: undefined,
+}
+
 window.addEventListener('load', setCanvasSize);
 // Para evitar tener que recargar el juego cada vez que la pantalla cambie de tamaño, agregago un eventListener para que se haga el cálculo automático del tamaño de la pantalla y de esta manera sea más responsive.
 window.addEventListener('resize', setCanvasSize);
-window.addEventListener('keydown', moveKeyboard)
-
-btnUp.addEventListener('click', moveUp)
-btnLeft.addEventListener('click', moveLeft)
-btnRight.addEventListener('click', moveRight)
-btnDown.addEventListener('click', moveDown)
-
-function moveUp() {
-    console.log('Moverse arriba');
-}
-
-function moveLeft() {
-    console.log('Moverse a la izquierda');
-}
-
-function moveRight() {
-    console.log('Moverse a la derecha');
-}
-
-function moveDown() {
-    console.log('Moverse abajo');
-}
-
-function moveKeyboard (event) {
-    switch (event.key) {
-        case "ArrowUp":
-            moveUp();
-            break;
-        case "ArrowLeft":
-            moveLeft();
-            break;
-        case "ArrowRight":
-            moveRight();
-            break;
-        case "ArrowDown":
-            moveDown();
-            break;
-    }
-}
 
 let canvasSize;
 let elementsSize;
@@ -59,7 +25,7 @@ function setCanvasSize() {
     }
     canvas.setAttribute('width', canvasSize)
     canvas.setAttribute('height', canvasSize)
-    
+
     elementsSize = canvasSize / 10;
 
     startGame();
@@ -76,18 +42,91 @@ function startGame() {
     const mapCols = mapRow.map(row => row.trim().split(''));
 
     // Alternativa al ciclo for para una mejor legibilidad
+    // Para que el jugador no se duplique al moverlo, hacemos un clearRect antes de mover al jugador
+    game.clearRect(0, 0, canvasSize, canvasSize);
     mapCols.forEach((row, rowI) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col];
             const posX = elementsSize * (colI + 1) + 7;
             const posY = elementsSize * (rowI + 1) - 7;
+
+            if (col == "O") {
+                if (!playerPosition.x && !playerPosition.y) {
+                    playerPosition.x = posX;
+                    playerPosition.y = posY;
+                    console.log({ playerPosition });
+                }
+            }
+            if (playerPosition.x < elementsSize) {
+                playerPosition.x += elementsSize;
+            } else if (playerPosition.y < 0) {
+                playerPosition.y += elementsSize; 
+            } else if (playerPosition.y > canvasSize) {
+                playerPosition.y -= elementsSize;
+            } else if (playerPosition.x > canvasSize + 7) {
+                playerPosition.x -= elementsSize;
+            }
             game.fillText(emoji, posX, posY);
         })
     })
 
+    movePlayer();
     // for (let row = 1; row <= 10; row++) {
     //     for (let col = 1; col <= 10; col++) {
     //         game.fillText(emojis[mapCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
     //     }
     // }
+}
+
+function movePlayer() {
+    game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+    console.log( {playerPosition} )
+}
+
+window.addEventListener('keydown', moveKeyboard);
+
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+function moveUp() {
+    console.log('Moverse arriba');
+    playerPosition.y -= elementsSize;
+    startGame()
+}
+
+function moveLeft() {
+    console.log('Moverse a la izquierda');
+    playerPosition.x -= elementsSize;
+    startGame()
+}
+
+function moveRight() {
+    console.log('Moverse a la derecha');
+    playerPosition.x += elementsSize;
+    startGame()
+}
+
+function moveDown() {
+    console.log('Moverse abajo');
+    playerPosition.y += elementsSize;
+    startGame()
+}
+
+function moveKeyboard(event) {
+    switch (event.key) {
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight":
+            moveRight();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+    }
 }
